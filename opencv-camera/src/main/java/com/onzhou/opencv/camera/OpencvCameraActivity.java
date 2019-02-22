f9777c89ca98a97d3c39986ac8fc0a60bb52ebef
Andy <byhook@163.com>
2019-02-22 16:20:09 +0800

    初步添加相机操作模块

---
+++ opencv-camera/src/main/java/com/onzhou/opencv/camera/OpencvCameraActivity.java
@@ -0,0 +1,19 @@
+package com.onzhou.opencv.camera;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @anchor: andy
+ * @date: 2019-02-22
+ * @description:
+ */
+public class OpencvCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
